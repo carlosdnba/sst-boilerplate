f4f64699a6c58ca73e0405005f07da1cd278a98f
Carlos Daniel <carlos.daniel@devsquad.email>
2022-05-13 18:16:04 -0300

    feat: export client

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const client = axios.create({
+export const client = axios.create({
   baseURL: process.env.REACT_APP_API_GATEWAY_URL,
   headers: {
     'Content-Type': 'application/json',
